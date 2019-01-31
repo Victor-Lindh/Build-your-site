9f83a8285016257ad107a6bfe385642f7b76e0bb
Victor Lindh <Victor.Lindh@yh.nackademin.se>
2019-01-31 09:20:09 +0100

    accordion-js

---
+++ accordion.js
@@ -0,0 +1,24 @@
+document.getElementById("header").addEventListener("click", function(){
+document.getElementById("menu2").classList.toggle("show");
+});
+document.getElementById("text1").addEventListener("click", function(){
+document.getElementById("textmenu1").classList.toggle("show");
+});
+document.getElementById("text2").addEventListener("click", function(){
+document.getElementById("textmenu2").classList.toggle("show");
+});
+document.getElementById("text3").addEventListener("click", function(){
+document.getElementById("textmenu3").classList.toggle("show");
+});
+document.getElementById("text4").addEventListener("click", function(){
+document.getElementById("textmenu4").classList.toggle("show");
+});
+document.getElementById("navbar").addEventListener("click", function(){
+document.getElementById("menu3").classList.toggle("show");
+});
+document.getElementById("main").addEventListener("click", function(){
+document.getElementById("menu4").classList.toggle("show");
+});
+document.getElementById("footer").addEventListener("click", function(){
+document.getElementById("menu5").classList.toggle("show");
+});
